0449abaf50a2ff3083ebef8ce1bc43a7c933301a
Asus <islomovgayrat5@gmail.com>
2023-09-12 12:54:04 +0500

    updated.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:patterns_bloc/bloc/list_post_cubit.dart';
 import 'package:patterns_bloc/pages/create_page.dart';
 import 'package:patterns_bloc/views/view_of_home.dart';
 
 import '../model/post_model.dart';
-import '../services/http_service.dart';
-import '../views/item_of_post.dart';
-import '../views/view_of_error.dart';
-import '../views/view_of_loading.dart';
-import '../views/view_of_postlist.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
